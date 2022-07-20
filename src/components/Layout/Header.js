b3f3114fb86046e2eea749e3542d24e5d3db7d3c
PeiYun <ollylee8520@gmail.com>
2022-07-20 12:25:45 -0600

    update the events project in profolio

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,10 +9,6 @@ export default function Header() {
             <a href='/portfolio'>Portfolio</a>
             <a href='/contact'>Contact</a>
             <a href='/resume'>Resume</a>
-            {/* <Link className="Nav__link" to="/">PeiYun Hite</Link>
-            <Link className="Nav__link" to="/">Portfolio</Link>
-            <Link className="Nav__link" to="/">Contact</Link>
-            <Link className="Nav__link" to="/">Resume</Link> */}
         </div>
         {/* <a target='_blank' onClick={() => window.location = 'mailto:ollyhite8520@gmail.com'}>ollyhite8520@gmail.com</a> */}
         <a href='mailto:ollyhite8520@gmail.com' target='_blank' rel='noreferrer'>ollyhite8520@gmail.com</a>
--- src/views/Portfolio.js
+++ src/views/Portfolio.js
@@ -1,63 +1,106 @@
-import React from 'react'
-import View from '../components/Layout/View'
-import watzPro from '../components/images/watz_img.png'
-import faithPro from '../components/images/faithplaceoutdoor_img.png'
-import rhythmPro from '../components/images/Rhythm-and-Booze-screenshot.png'
-import rosePro from '../components/images/Flores_Rosa_Emlia_screenshot.png'
-import {FaGithub} from 'react-icons/fa/'
-
+import React from "react";
+import View from "../components/Layout/View";
+import watzPro from "../components/images/watz_img.png";
+import faithPro from "../components/images/faithplaceoutdoor_img.png";
+import rhythmPro from "../components/images/Rhythm-and-Booze-screenshot.png";
+import rosePro from "../components/images/Flores_Rosa_Emlia_screenshot.png";
+import eventPro from "../components/images/events.screenshot.png";
+import { FaGithub } from "react-icons/fa/";
 
 export default function Portfolio() {
-    return (
-        <View>
-            <div className='project-frame'>
-            <h1 className='title'>Portfolio</h1>
-                <div className="img">
-                    <img src={watzPro} alt="watz-project images"/>
-                    <p className="shadow"></p>
-                    <div className='project-info'>
-                        <div className='title-frame'>
-                            <h3>WATZ</h3>
-                            <span><a href='https://github.com/ollyhite/proj-watz' target='_blank' rel="noreferrer"><FaGithub /></a></span>
-                        </div>
-                        <p>Stocks Shopping Websites</p>
-                    </div>
-                </div>
-                <div className="img">
-                    <img src={faithPro} alt="FaithPlaceOutdoor-project images"/>
-                    <p className="shadow"></p>
-                    <div className='project-info'>
-                        <div className='title-frame'>
-                            <h3>FaithPlaceOutdoors</h3>
-                            {/* <span><a href='/' target='_blank' rel="noreferrer"><FaGithub /></a></span> */}
-                        </div>
-                        <p>Non-Profits Website</p>
-                    </div>
-                </div>
-                <div className="img">
-                    <img src={rhythmPro} alt="RhythmAndBooze-project images"/>
-                    <p className="shadow"></p>
-                    <div className='project-info'>
-                        <div className='title-frame'>
-                            <h3>Rhythm and Booze</h3>
-                            <span><a href='https://github.com/ollyhite/Rhythm-and-Booze' target='_blank' rel="noreferrer"><FaGithub /></a></span>
-                        </div>
-                        <p>Suggection about wine and muisc for you</p>
-                    </div>
-                </div>
-                <div className="img">
-                    <img src={rosePro} alt="Rose-project images"/>
-                    <p className="shadow"></p>
-                    <div className='project-info'>
-                        <div className='title-frame'>
-                            <h3>Flores Rosa Emilia</h3>
-                            <span><a href='https://github.com/ollyhite/Flores-Rosa-Emilia' target='_blank' rel="noreferrer"><FaGithub /></a></span>
-                        </div>
-                        <p>Online flower store</p>
-                    </div>
-                </div>
-                <p>2022 By Pei-Yun Hite</p>
+  return (
+    <View>
+      <div className="project-frame">
+        <h1 className="title">Portfolio</h1>
+        <div className="img">
+          <img src={watzPro} alt="watz-project images" />
+          <p className="shadow"></p>
+          <div className="project-info">
+            <div className="title-frame">
+              <h3>WATZ</h3>
+              <span>
+                <a
+                  href="https://github.com/ollyhite/proj-watz"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithub />
+                </a>
+              </span>
             </div>
-        </View>
-    )
+            <p>Stocks Shopping Websites</p>
+          </div>
+        </div>
+        <div className="img">
+          <img src={faithPro} alt="FaithPlaceOutdoor-project images" />
+          <p className="shadow"></p>
+          <div className="project-info">
+            <div className="title-frame">
+              <h3>FaithPlaceOutdoors</h3>
+              {/* <span><a href='/' target='_blank' rel="noreferrer"><FaGithub /></a></span> */}
+            </div>
+            <p>Non-Profits Website</p>
+          </div>
+        </div>
+        <div className="img">
+          <img src={rhythmPro} alt="RhythmAndBooze-project images" />
+          <p className="shadow"></p>
+          <div className="project-info">
+            <div className="title-frame">
+              <h3>Rhythm and Booze</h3>
+              <span>
+                <a
+                  href="https://github.com/ollyhite/Rhythm-and-Booze"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithub />
+                </a>
+              </span>
+            </div>
+            <p>Suggection about wine and muisc for you</p>
+          </div>
+        </div>
+        <div className="img">
+          <img src={rosePro} alt="Rose-project images" />
+          <p className="shadow"></p>
+          <div className="project-info">
+            <div className="title-frame">
+              <h3>Flores Rosa Emilia</h3>
+              <span>
+                <a
+                  href="https://github.com/ollyhite/Flores-Rosa-Emilia"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithub />
+                </a>
+              </span>
+            </div>
+            <p>Online flower store</p>
+          </div>
+        </div>
+        <div className="img">
+          <img src={eventPro} alt="evets-project images" />
+          <p className="shadow"></p>
+          <div className="project-info">
+            <div className="title-frame">
+              <h3>DU-Events-Listener</h3>
+              <span>
+                <a
+                  href="https://github.com/ruxpin86/DU-Event-Listeners"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithub />
+                </a>
+              </span>
+            </div>
+            <p>The Web can keep in touch, chat, share coding stuffs</p>
+          </div>
+        </div>
+        <p>2022 By Pei-Yun Hite</p>
+      </div>
+    </View>
+  );
 }
